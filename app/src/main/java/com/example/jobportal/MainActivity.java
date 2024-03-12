fa9b6b4498ebf72b82b2b0fbde1c3b71ff2913fb
karan-dp <exam@202stud130.SVV.local>
2024-03-12 14:12:21 +0530

    home page added

--- app/src/main/java/com/example/jobportal/MainActivity.java
+++ app/src/main/java/com/example/jobportal/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.jobportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginBtn,signUpRedirectBtn;
+    EditText loginPwd,loginEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn=findViewById(R.id.loginBtn);
+        signUpRedirectBtn=findViewById(R.id.signUpRedirectBtn);
+
+        loginPwd=findViewById(R.id.loginPwd);
+        loginEmail=findViewById(R.id.loginEmail);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // redirect to home activity
+                Intent intent = new Intent(MainActivity.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpRedirectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // redirect to signUpPage activity
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
